d502ff65fc1757fb15c6d9e03969d4e30bb5eec7
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-11-08 00:30:16 +0600

    updated nginx.conf

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,17 @@
 
 echo "Создание файла .env..."
 cp .env.sample .env
+echo "Делаем dump БД..."
+sudo docker-compose exec -T pg-14 sh -c 'exec pg_dump -U postgres --inserts memorybase > /backup/dump.sql'
 echo "Обновляем образы..."
 sudo docker-compose build
 echo "Останавливаем и удаляем контейнеры..."
 sudo docker-compose down
+echo "Поднимаем базу и восстанавливаем..."
+sudo docker-compose up -d pg-14
+sudo docker-compose exec pg-14 sh -c 'exec psql -U postgres memorybase < db/db-backups/dump.sql'
 echo "Запускаем новые контейнеры..."
 sudo docker-compose up -d
+
 echo "Удаляем всё кроме зависимостей pg и файла docker-compose..."
 find . ! \( -path './db' \) ! \( -name 'docker-compose.yml' \) ! \( -name '.env' \) -delete || true
