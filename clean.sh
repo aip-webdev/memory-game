3f25ce0ddd2cc7569bf2270ee0d968f8161c96cb
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-11-07 23:22:14 +0600

    add clean and deploy files

---
+++ clean.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Удаление старых контейнеров..."
+CONTAINERS=$(sudo docker ps -a -f status=exited -q)
+echo "Containers: $CONTAINERS"
+sudo docker rm "$CONTAINERS"
+echo "Удаление неиспользуемых образов..."
+IMAGES=$(sudo docker images -f "dangling=true" -q)
+echo "Images: $IMAGES"
+sudo docker rmi "$IMAGES"
+
+if [ ! -d memory-game ]; then
+    echo "Папка memory-game отсутствует. Создание новой папки..."
+    mkdir memory-game
+    sudo chmod 777 memory-game
+    echo "Папка memory-game создана успешно."
+else
+    echo "Папка memory-game уже существует."
+    cd memory-game || exit
+    sudo docker-compose down
+    sudo find . ! \( -path './db/db-data' \) -delete
+fi
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Создание файла .env..."
+cp .env.sample .env
+echo "Обновляем образы..."
+sudo docker-compose build
+echo "Останавливаем и удаляем контейнеры..."
+sudo docker-compose down
+echo "Запускаем новые контейнеры..."
+sudo docker-compose up -d
+echo "Удаляем всё кроме зависимостей pg и файла docker-compose..."
+find . ! \( -path './db' \) ! \( -name 'docker-compose.yml' \) -delete
