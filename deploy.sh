d4d672ff89141352052551980483c2ed77d91d54
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-11-08 20:11:35 +0600

    deploy.sh clean

--- deploy.sh
+++ deploy.sh
@@ -14,22 +14,6 @@ delete_files_except() {
 echo -e "${GREEN}Создание файла .env..."
 cp .env.sample .env
 
-echo -e "${GREEN}Проверка базы данных..."
-if docker ps -f "name=mg-pg" --format '{{.Names}}' | grep -q "mg-pg"; then
-    echo -e "${GREEN}Контейнер mg-pg уже запущен."
-    #echo -e "${GREEN}Создание дампа БД..."
-    #sudo docker compose exec -T pg-16 sh -c "exec pg_dump -U postgres --inserts memorybase > /backup/dump.sql"
-    #sudo chmod 777 db/db-backups
-    #(cp ./db/db-backups/dump.sql ./db/dump.sql && sudo rm -rf ./db/db-backups/dump.sql ) || true
-else
-    echo -e "${GREEN}Контейнер mg-pg не запущен. Запуск контейнера с БД..."
-    docker compose up -d pg-16
-fi
-
-echo -e "${GREEN}Проверка adminer..."
-docker ps -f "name=mg-adminer" --format '{{.Names}}' | grep -q "mg-adminer" || docker compose up -d adminer
-
-
 echo -e "${GREEN}Обновление образов docker..."
 sudo docker compose build server & sudo docker compose build nginx
 wait
