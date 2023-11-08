dc083a5fa817377fb1b62fc3268b648a73f17b54
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-11-08 16:41:33 +0600

    scripts updated

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ cp .env.sample .env
 
 echo -e "${GREEN}Проверка базы данных..."
 if docker ps -f "name=mg-pg" --format '{{.Names}}' | grep -q "mg-pg"; then
-    echo -e "${GREEN}Контейнер mg-pg уже запущен. Создание дампа БД..."
-    sudo docker compose exec -T pg-14 sh -c "exec pg_dump -U postgres --inserts memorybase > /backup/dump.sql"
+    echo -e "${GREEN}Контейнер mg-pg уже запущен."
+    #echo -e "${GREEN}Создание дампа БД..."
+    #sudo docker compose exec -T pg-14 sh -c "exec pg_dump -U postgres --inserts memorybase > /backup/dump.sql"
     #sudo chmod 777 db/db-backups
     #(cp ./db/db-backups/dump.sql ./db/dump.sql && sudo rm -rf ./db/db-backups/dump.sql ) || true
 else
@@ -29,11 +30,11 @@ sudo docker compose down server & sudo docker compose down nginx
 wait
 echo -e "${GREEN}Остановка и удаление контейнеров - SUCCESS"
 
-echo -e "${GREEN}Удаление старых контейнеров..."
-CONTAINERS=$(sudo docker ps -a -f status=exited -q)
-echo -e "${GREEN}Containers: $CONTAINERS"
-sudo docker rm "$CONTAINERS" || true
-
+#echo -e "${GREEN}Удаление старых контейнеров..."
+#CONTAINERS=$(sudo docker ps -a -f status=exited -q)
+#echo -e "${GREEN}Containers: $CONTAINERS"
+#sudo docker rm "$CONTAINERS" || true
+#
 #echo -e "${GREEN}Восстановление БД..."
 #(cp ./db/dump.sql ./db/db-backups/dump.sql && sudo rm -rf ./db/dump.sql ) || true
 #sudo rm -rf ./db/db-data
@@ -41,8 +42,8 @@ sudo docker rm "$CONTAINERS" || true
 #sudo docker compose exec pg-14 sh -c 'exec psql -U postgres memorybase < /backup/dump.sql'
 
 echo -e "${GREEN}Запуск новых контейнеров..."
-sudo docker compose up -d server & sudo docker compose up -d nginx
-wait
+sudo docker compose up -d server
+sudo docker compose up -d nginx
 
 echo -e "${GREEN}Удаление ненужных файлов..."
 find . ! \( -path './db' \) ! \( -name 'docker-compose.yml' \) ! \( -name '.env' \) -delete || true
