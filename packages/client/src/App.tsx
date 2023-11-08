e2c80facceac1abff81b365e1dc55d0ab26a30a3
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-11-08 20:22:23 +0600

    fix icon & service worker

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -17,7 +17,7 @@ function startServiceWorker() {
   window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-        .register(`${REDIRECT_URI}/service-worker.js`)
+        .register(`${REDIRECT_URI}/assets//service-worker.js`)
         .then(registration => {
           console.log('SW registration successful: ', registration.scope)
         })
