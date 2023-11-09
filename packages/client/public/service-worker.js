391c0b6b411c6fbdc0da9bc6c02633d4163f6770
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-11-09 10:52:36 +0600

    sw route fix

--- packages/client/public/service-worker.js
+++ packages/client/public/service-worker.js
@@ -1,59 +1,59 @@
-const CACHENAME = 'cache-v1'
+const CACHE_NAME = 'cache-v1'
 const resources = ['/']
 
-self.addEventListener('install', event => {
+this.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHENAME)
-      .then(cache => cache.addAll(resources))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(resources))
   )
 })
 
-const MAXAGE = 3600
+const MAX_AGE = 3600
 
-const fetchAndUpdateCache = (request) => {
-  return fetch(request, { cache: 'no-store' }).then(response => {
-    if (!response || response.status !== 200 || !response.url.includes('https')) {
-      return response
-    }
-    const responseToCache = response.clone()
-    caches.open(CACHENAME).then(cache => {
-      if (request.method === 'GET') {
-
-        cache.put(request, responseToCache)
-      } else if (['POST', 'PUT'].includes(request.method)) {
-        return (
-          !request.url.includes('leaderboard/all') ||
-          !request.url.includes('user-theme')
-        ) ? response : cache.add(request)
-      }
-    })
-    return response
-  })
-}
-
-self.addEventListener('fetch', event => {
+this.addEventListener('fetch', event => {
   event.respondWith(
+    // ищем запрошенный ресурс
     caches.match(event.request).then(cachedResponse => {
-      const request = event.request.clone()
-      if (!cachedResponse) {
-        return fetchAndUpdateCache(request)
-      }
-      const lastModified = new Date(cachedResponse.headers.get('last-modified'))
-      if (lastModified && Date.now() - lastModified.getTime() > MAXAGE * 1000) {
-        return fetchAndUpdateCache(request)
-      }
-      return cachedResponse
-    }).catch((e) => new Response(`Something went wrong: ${e}`))
-  )
-})
+      let lastModified
+      const fetchRequest = event.request.clone()
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => cacheName !== CACHENAME)
-          .map(cacheName => caches.delete(cacheName))
-      )
+      // если ресурса нет в кэше
+      if (!cachedResponse) {
+        return fetch(fetchRequest, { cache: 'no-store' }).then(response => {
+          const responseClone = response.clone()
+
+          updateCache(event.request, responseClone)
+          return response
+        })
+      }
+
+      // если ресурс есть в кэше
+      lastModified = new Date(cachedResponse.headers.get('last-modified'))
+      // если ресурс устаревший
+      if (lastModified && Date.now() - lastModified.getTime() > MAX_AGE) {
+        return fetch(fetchRequest, { cache: 'no-store' })
+          .then(response => {
+            if (!response || response.status >= 500) {
+              return cachedResponse
+            }
+            updateCache(event.request, response.clone())
+            return response
+          })
+          .catch(() => cachedResponse)
+      }
+
+      return cachedResponse
     })
   )
 })
+
+const updateCache = (req, res) => {
+  if (req.url.includes('https')) {
+    caches.open(CACHE_NAME).then(cache => {
+      if (req.method === 'GET') {
+        cache.put(req, res)
+      } else if (req.url.includes('leaderboard/all') || req.url.includes('user-theme')) {
+        cache.add(req)
+      }
+    })
+  }
+}
